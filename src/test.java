c9c62389535f9295f884a9e873b9ec5020cccc80
MinhTriet <minhtriet4519@gmail.com>
2024-10-23 01:22:43 +0700

    Second commit

--- src/test.java
+++ src/test.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
 public class test {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
         DSNV ds = new DSNV();
         ds.nhapDS();
         System.out.println("Danh sach nhan vien:");
         ds.xuatDS();
         System.out.println("Luong cac nhan vien:");
         ds.xuatluong();
-        sc.close();
     }
 }
